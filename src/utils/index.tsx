0b00e7dd63988875c7257ce6eba6eef86bc5e984
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-04-18 12:34:05 +0800

    Moved truncate function to utils folder

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -23,3 +23,12 @@ export interface IPostUrl {
   url: string;
   slug?: string;
 }
+
+export const MAX_LENGTH = 140;
+
+export const truncateUrl = (url: string, MAX_LENGTH: number) => {
+  if (url.length > MAX_LENGTH) {
+    return url.slice(0, MAX_LENGTH - 3) + "...";
+  }
+  return url;
+};
